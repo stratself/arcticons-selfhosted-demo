9a0505918bef914e6e9d3dd3713cd02e9d6cfc62
stratself <126093083+stratself@users.noreply.github.com>
2024-12-16 13:58:16 -0500

    HUGE CHANGES - Add dynamic button generation from appfilter categories and corresponding counters
    The publish-website.py script now generate filter buttons from categories inside the appfilter.json. It also counts total and category-specific number of icons in its build

--- scripts/publish-website.py
+++ scripts/publish-website.py
@@ -3,18 +3,20 @@ import json
 iconRawUrl = 'https://raw.githubusercontent.com/skedastically/arcticons-selfhosted-demo/refs/heads/master'
 siteTemplateFile = 'scripts/template/template.html'
 iconTemplateFile = 'scripts/template/iconTemplate.html'
+buttonToggleFile = "scripts/template/buttonToggleTemplate.html"
 iconCategoryMap = 'newicons/appfilter.json'
 websiteFile = 'docs/index.html'
 svgLink = iconRawUrl + '/icons/white/svg/'
 pngLink = iconRawUrl + '/icons/white/png/'
 webpLink = iconRawUrl + '/icons/white/webp/'
 
-def publishWebsite(siteTemplateFile, iconTemplateFile, iconCategoryMap, websiteFile):
+def publishWebsite(siteTemplateFile, iconTemplateFile, iconCategoryMap, websiteFile, buttonToggleFile):
     
-    iconTemplate = open(iconTemplateFile,'r').read()
     tagmap = json.load(open(iconCategoryMap,"r"))
     icons = [i for i in tagmap]
+    iconTemplate = open(iconTemplateFile,'r').read()
     iconDivs = ""
+    categories = []
 
     for icon in icons:
         print(f"Exporting {icon} to website")
@@ -26,12 +28,25 @@ def publishWebsite(siteTemplateFile, iconTemplateFile, iconCategoryMap, websiteF
         iconDiv = iconDiv.replace("{pngLink}",pngLink)
         iconDiv = iconDiv.replace("{webpLink}",webpLink)
         iconDivs = iconDivs + iconDiv
+        for item in tagmap[icon]["categories"]:
+            categories.append(item)
+
+    buttonToggleTemplate = open(buttonToggleFile).read()
+    buttonToggleDiv = ""
+    for category in set(categories):
+        buttonToggle = buttonToggleTemplate.replace("{category}",category)
+        buttonToggle = buttonToggle.replace("{categoryTitle}",category.title())
+        buttonToggle = buttonToggle.replace("{categoryCount}",str(categories.count(category)))
+        buttonToggleDiv = buttonToggleDiv + buttonToggle + '\n'
 
     iconWebsite = open(siteTemplateFile).read()
-
+    iconWebsite = iconWebsite.replace("{iconCount}",str(len(icons)))
+    
     index = iconWebsite.find('<div class="iconList">') + len('<div class="iconList">')
-
     iconWebsite = iconWebsite[:index] + iconDivs + iconWebsite[index:]
+
+    index = iconWebsite.find('<div class="category-filters">') + len('<div class="category-filters">')
+    iconWebsite = iconWebsite[:index] + "\n\t<text>Categories:</text>" + buttonToggleDiv + iconWebsite[index:]
 
     f = open(websiteFile,'w')
     print("Writing index.html")
@@ -40,4 +55,4 @@ def publishWebsite(siteTemplateFile, iconTemplateFile, iconCategoryMap, websiteF
     print(f"Completed publishing website! {len(icons)} icons are generated")
 
 if __name__ == "__main__":
-    publishWebsite(siteTemplateFile,iconTemplateFile,iconCategoryMap,websiteFile)
+    publishWebsite(siteTemplateFile,iconTemplateFile,iconCategoryMap,websiteFile,buttonToggleFile)
