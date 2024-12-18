54951326858c5b2653ad67daa888c514950f9faf
skedastically <skedastically@proton.me>
2024-12-18 15:19:37 -0500

    replace underscore to whitespace for proper search

--- docs/js/search.js
+++ docs/js/search.js
@@ -3,8 +3,8 @@ document.getElementById('searchBar').addEventListener('input', function (event) 
     const listIcons = document.querySelectorAll('.iconList .iconPanel');
     
     listIcons.forEach(function (icon) {
-        const iconID = icon.id.toLowerCase();
-        if (iconID.includes(searchTerm)) {
+        const iconName = icon.id.toLowerCase().replace("_"," ");
+        if (iconName.includes(searchTerm)) {
             icon.style.display = 'flex';
         } else {
             icon.style.display = 'none';
