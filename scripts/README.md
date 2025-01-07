c1e532bd2554382f314f62476f94cb8d271d3c82
stratself <126093083+stratself@users.noreply.github.com>
2025-01-07 10:02:13 -0500

    check-appfilter script now checks for either
    - both src (default: ./newicons) and dst (default: ./icons/<flavor-name>/<format>) paths for each flavor inside config file, or
    - a comma-separated list of formats
    
    against the json appfilter
    
    this gives high flexibility for both custom and default flavors and can be used in CI flows
    
    * also add some comments for generate-icons.toml

--- scripts/README.md
+++ scripts/README.md
@@ -15,9 +15,14 @@ uv run ./scripts/publish-website.py
 
 Takes icons from `/newicons` and export them to svg, png, and webp files.
 
-This script is a heavily modified version of [`preparerelease.py`](https://github.com/Arcticons-Team/Arcticons/blob/main/scripts/preparerelease.py) found in the main repo. It takes the following arguments:
+This script is a heavily modified version of [`preparerelease.py`](https://github.com/Arcticons-Team/Arcticons/blob/main/scripts/preparerelease.py) found in the main repo. 
+
+<details>
+
+<summary>It takes the following arguments:</summary>
+
 ```
-usage: gen.py [-h] [-c CONFIG] [--checkonly] [--checksrc CHECKSRC] [--nopreserve]
+usage: generate-icons.py [-h] [-c CONFIG] [--checkonly] [--checksrc CHECKSRC] [--nopreserve]
 
 options:
   -h, --help            show this help message and exit
@@ -27,11 +32,40 @@ options:
   --checksrc CHECKSRC   Path to the icons directory for checking (only enabled alongside --checkonly flag)
   --nopreserve          Remove icons after creation
 ```
+
+</details>
+
 ## 2. check-appfilter.py
 
-Validate the JSON category map (`appfilter.json`) and auto-sort entries alphabetically. Currently only checks for files with missing category or without a reference.
+Validate the JSON category map (`appfilter.json`) against either 
+
+- both src (default: ./newicons) and dst (default: ./icons/<flavor-name>/<format>) paths for each flavor inside config file, or
+- a comma-separated list of formats
+
+and auto-sort entries alphabetically. Currently only checks for files with missing category or without a reference.
 
 If an alert is raised, kindly fix the issue before proceeding.
+
+
+<details>
+<summary>It takes the following arguments</summary>
+
+```
+usage: check-appfilter.py [-h] [--nosort] [-c CONFIG] [-p PATHS] JSON_APPFILTER
+
+positional arguments:
+  JSON_APPFILTER        Path to the JSON file
+
+options:
+  -h, --help            show this help message and exit
+  --nosort              Sort JSON keys alphabetically
+  -c CONFIG, --config CONFIG
+                        Config file to use
+  -p PATHS, --paths PATHS
+                        Comma-separated paths to the icons directory
+```
+
+</details>
 
 ## publish-website.py
 
--- scripts/check-appfilter.py
+++ scripts/check-appfilter.py
@@ -1,16 +1,58 @@
 import os
 import argparse
 import json
+import tomllib
+import jsonschema
+
+configSchema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "size": {"type": "integer"},
+        "color": {"type": "string"},
+        "src": {
+            "type": "object",
+            "properties": {"path": {"type": "string"}},
+            "required": ["path"],
+            "additionalProperties": False,
+        },
+        "dst": {
+            "type": "object",
+            "properties": {
+                "svg": {"type": "string"},
+                "png": {"type": "string"},
+                "webp": {"type": "string"},
+            },
+            "minProperties": 1,
+            "additionalProperties": False,
+        },
+    },
+    "required": ["size", "color", "src", "dst"],
+}
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--nosort", action="store_true", help="Sort JSON keys alphabetically")
-parser.add_argument("ICONS_PATH", type=str, help="Path to the icons directory")
-parser.add_argument("JSON_FILE", type=str, help="Path to the JSON file")
+parser.add_argument(
+    "--nosort", action="store_true", help="Sort JSON keys alphabetically"
+)
+parser.add_argument("-c", "--config", type=str, help="Config file to use")
+parser.add_argument(
+    "-p", "--paths", type=str, help="Comma-separated paths to the icons directory"
+)
+parser.add_argument("JSON_APPFILTER", type=str, help="Path to the JSON file")
 
 args = parser.parse_args()
 
 
-def check_icons(icons_path, categories_map_path):
+def get_config(config_file: str, configSchema: dict):
+    # Get config file and check their validity
+    config = tomllib.load(open(config_file, "rb"))
+    for i in config:
+        jsonschema.validate(instance=config[i], schema=configSchema)
+    return config
+
+
+def check_icons(icons_paths: list, categories_map_path: str) -> None:
     print("Checking validity of JSON categories map")
     categories_map = json.load(open(categories_map_path, "r"))
 
@@ -21,8 +63,12 @@ def check_icons(icons_path, categories_map_path):
             [icons.append(alt) for alt in categories_map[icon]["alts"]]
     print("There are", len(icons), "icons in the category map")
 
-    iconList = [i[:-4] for i in os.listdir(icons_path)]
-    print("There are", len(iconList), "icons in folder")
+    iconList = []
+    for icons_path in icons_paths:
+        print(icons_path)
+        iconsInPath = [icon[:-4] for icon in os.listdir(icons_path)]
+        print("There are", len(iconsInPath), f"icons in {icons_path}")
+        iconList += iconsInPath
 
     lostIcons = list(set(iconList) - set(icons))
     lostKeys = list(set(icons) - set(iconList))
@@ -46,13 +92,26 @@ def check_icons(icons_path, categories_map_path):
     if args.nosort:
         return
     print("Sorting JSON categories map")
-    json.dump(
-        categories_map, open(categories_map_path, "w"), indent=4, sort_keys=True
-    )
+    json.dump(categories_map, open(categories_map_path, "w"), indent=4, sort_keys=True)
+    return
 
 
 def main():
-    check_icons(args.ICONS_PATH, args.JSON_FILE)
+    if args.config is not None:
+        config = get_config(args.config, configSchema)
+        paths = []
+        for flavor in config:
+            paths.append(config[flavor]["src"]["path"])
+            # takes any first key that is a path
+            firstKey = next(iter(config[flavor]["dst"]))
+            paths.append(config[flavor]["dst"][firstKey])
+        print(paths)
+    elif args.paths is not None:
+        paths = args.paths.split(",")
+    else:
+        print("Please supply paths or a config file")
+        return
+    check_icons(paths, args.JSON_APPFILTER)
 
 
 if __name__ == "__main__":
